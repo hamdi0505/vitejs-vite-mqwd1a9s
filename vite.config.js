db8646cd223115f25e422be7b97101040f447565
hamdi0505 <hamdi01bey@gmail.com>
2026-03-10 02:11:14 +0100

    pwa2

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig({
         start_url: "/",
         scope: "/",
         icons: [
-          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" }
+          { src: "/pwa192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa512.png", sizes: "512x512", type: "image/png" }
         ]
       }
     })
